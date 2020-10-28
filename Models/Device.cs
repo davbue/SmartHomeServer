bb51b3ea19986eee721a0964fc01187a2820cb5f
davbue <davbue@bluewin.ch>
2020-10-28 15:51:38 +0100

    Add project files.

---
+++ Models/Device.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartHomeServer.Models
+{
+    public class Device
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsUsed { get; set; }
+    }
+}
---
+++ Models/SmartHomeContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.EntityFrameworkCore.Extensions;
+
+namespace SmartHomeServer.Models
+{
+    public class SmartHomeContext : DbContext
+    {
+        public DbSet<Device> Devices { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=SmartHome;user=root;password=Ebnilinde.17");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Device>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired();
+                entity.Property(e => e.IsUsed);
+            });
+        }
+    }
+}
